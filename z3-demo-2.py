778cbd362b69ebbcec48963c7d0f3a53a9662a1c
NavidNaf <navidfazlerabbi@iut-dhaka.edu>
2024-10-14 02:28:31 +0600

    added comments and installed ssh

--- z3-demo-2.py
+++ z3-demo-2.py
@@ -1,4 +1,4 @@
-#This program uses the Z3 solver to define integer variables `x` and `y` and applies the `simplify()` function to various expressions. It first simplifies the arithmetic expression `x + y + 2*x + 3`, reducing it to a simpler form. Next, it simplifies the logical comparison `x < y + x + 2`. Finally, it simplifies a compound logical expression involving an inequality and a sum of squares, combining and reducing the terms within the `And` condition. The `simplify()` function helps reduce complex expressions into their most straightforward form in each case.
+# This program uses the Z3 solver to define integer variables `x` and `y` and applies the `simplify()` function to various expressions. It first simplifies the arithmetic expression `x + y + 2*x + 3`, reducing it to a simpler form. Next, it simplifies the logical comparison `x < y + x + 2`. Finally, it simplifies a compound logical expression involving an inequality and a sum of squares, combining and reducing the terms within the `And` condition. The `simplify()` function helps reduce complex expressions into their most straightforward form in each case.
 
 from z3 import *
 
